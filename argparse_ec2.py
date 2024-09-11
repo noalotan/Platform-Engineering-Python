30bca6bf48c01f13061c542c8208565ee5e141dc
noalotan <noalotan1@gmail.com>
2024-09-12 00:19:11 +0300

    manage_ec2.py corrections

--- argparse_ec2.py
+++ argparse_ec2.py
@@ -1,11 +1,12 @@
 import argparse
-from manage_ec2 import create_ec2_instance
+from manage_ec2 import create_ec2_instance, list_instances, start_instances, stop_instances
 
 parser = argparse.ArgumentParser()
 parser.add_argument("set", help="manage instances", choices=["create", "manage", "list"])
 parser.add_argument("--image", help="image of the instance", choices=["ubuntu", "amazon linux"])
 parser.add_argument("--type", help="type of instance", choices=["t3.nano", "t4.nano"])
 parser.add_argument("--action", help="action for managing", choices=['start', 'stop'])
+parser.add_argument("--id", help="instance id")
 args = parser.parse_args()
 
 if args.set == 'create':
@@ -15,18 +16,20 @@ if args.set == 'create':
             image_id = 'ami-0e86e20dae9224db8'
         else:
             image_id = 'ami-0e86e20dae9224db8'
-
         instance_type = args.type
-
         create_ec2_instance(image_id, instance_type)
     else:
         print(f"instance type/image is missing.")
 
 elif args.set == 'manage':
-    if args.action is None:
-        print(f"action is missing")
+    if args.action and args.id:
+        if args.action == 'start':
+            start_instances(args.id)
+        elif args.action == 'stop':
+            stop_instances(args.id)
     else:
-        print(f"managing instance with {args.action}")
+        print(f"action or id is missing")
 
 elif args.set == 'list':
     print(f"listing the instances")
+    list_instances()
--- manage_ec2.py
+++ manage_ec2.py
@@ -1,14 +1,11 @@
-# this code creates ec2 instances
 import boto3
-all_ec2 = {}
-
 
 def create_ec2_instance(image_id, instance_type):
     ec2_client = boto3.resource('ec2')
     instance = ec2_client.create_instances(
         ImageId=image_id,
         InstanceType=instance_type,
-        KeyName='noa-python-project.pem',
+        KeyName='noa-python-project',
         SecurityGroupIds=['sg-050e5bfa643d29369'],
         SubnetId='subnet-0ef884acbd166c8c4',
         MinCount=1,
@@ -16,7 +13,7 @@ def create_ec2_instance(image_id, instance_type):
         TagSpecifications=[
             {'ResourceType': 'instance',
              'Tags': [
-                 {'Key': 'Platform Engineering', 'Value': 'True'}
+                 {'Key': 'Platform', 'Value': 'True-Noa'}
              ]
              }
         ]
@@ -24,17 +21,30 @@ def create_ec2_instance(image_id, instance_type):
     print(f"\nInitiating instance now, please wait...")
     instance[0].wait_until_running()
     print(f"instance {instance[0].instance_id} was created successfully!\n")
-    all_ec2[instance[0].instance_id] = instance[0]
-    return all_ec2
+
+
+
 
 
 def list_instances():
-    for instance in all_ec2:
-        print(instance)
-
-
-def stop_ec2_instance(instance_id):
     ec2_client = boto3.client('ec2')
-    response = ec2_client.stop_instances(InstanceIds=[instance_id])
-    print(f"Stopping instance {instance_id}.")
-    print(response)
+    instances = ec2_client.describe_instances(
+        Filters=[{'Name': 'tag:Platform',
+                   'Values': ['True-Noa']}])
+    for reservation in instances['Reservations']:
+        for instance in reservation['Instances']:
+            print(instance['InstanceId'])
+
+
+def stop_instances(instance_id):
+    ec2_client = boto3.client('ec2')
+    instance_ids = [instance_id]
+    instances = ec2_client.stop_instances(InstanceIds=instance_ids)
+    print(f"stopping the instance {instance_id}")
+
+
+def start_instances(instance_id):
+    ec2_client = boto3.client('ec2')
+    instance_ids = [instance_id]
+    instances = ec2_client.start_instances(InstanceIds=instance_ids)
+    print(f"starting the instance {instance_id}")
